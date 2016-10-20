4e15febbb1c1d20d53ee607d10a40c5e00fbb7fb
Ester Ohman <ohmane@uw.edu>
2016-10-20 15:03:46 -0700

    added new line to file

--- challenge3/js/app.js
+++ challenge3/js/app.js
@@ -2,3 +2,7 @@
 app.js - application script for the movies challenge
 add your code to this file
 */
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document Ready");
+});
