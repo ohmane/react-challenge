61fb4cc53edab5b640fdee37e67f8c8d89770919
Ester Ohman <ohmane@uw.edu>
2016-11-15 00:18:27 -0800

    Update weather.js
    added wind speed and country

--- challenge5/weather.js
+++ challenge5/weather.js
@@ -5,14 +5,21 @@ class Weather extends React.Component {
         }
         
         return (
-            <div>
-                <h2>{this.props.location}</h2>
+            <div className="search_results">
+                <h2>{this.props.name}, {this.props.country}</h2>
 
-                <div>
-                    <img src={this.props.weather.icon} />
+                <div className="weather">
+                    <img src={this.props.icon} />
+                    <p>Weather: {this.props.main} </p>
+                    <p>Description: {this.props.description} </p>
+                    <p>Temperature: {this.props.temp} &#8457;</p>
+                    <p>Minimum Temperature: {this.props.temp_min} 	&#8457;</p>
+                    <p>Maximum Temperature: {this.props.temp_max} 	&#8457;</p>
+                    <p>Humidity: {this.props.humidity}% </p>
+                    <p>Wind Speed: {this.props.speed} miles per second </p>
                 </div>
 
-                <button onClick={(e) => this.save(e)}>Save</button>
+                <button className= "save-button" onClick={(e) => this.save(e)}>Save</button>
             </div>
         );
     }
